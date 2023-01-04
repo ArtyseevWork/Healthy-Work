7d440d4d36ad024ec2799f919e6d126a0380aeaf
ArtyseevWork <ArtyseevWork@gmail.com>
2023-01-04 17:07:52 +0200

    feat (MainActivity) add back button handler

--- app/src/main/java/com/mordansoft/healthywork/MainActivity.java
+++ app/src/main/java/com/mordansoft/healthywork/MainActivity.java
@@ -2,13 +2,35 @@ package com.mordansoft.healthywork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static long back_pressed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public void onBackPressed() {   //exit from app
+        if (back_pressed + 2000 > System.currentTimeMillis()) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getBaseContext(), getString(R.string.activity_main_exit_message),
+                    Toast.LENGTH_SHORT).show();
+            back_pressed = System.currentTimeMillis();
+        }
+    }
+
+
 }
