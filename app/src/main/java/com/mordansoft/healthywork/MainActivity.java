f2f14be09413c5d7f486745dcbfd5cd9365dba34
ArtyseevWork <ArtyseevWork@gmail.com>
2023-01-04 17:17:00 +0200

    feat (MainActivity) add binding

--- app/src/main/java/com/mordansoft/healthywork/MainActivity.java
+++ app/src/main/java/com/mordansoft/healthywork/MainActivity.java
@@ -6,20 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.mordansoft.healthywork.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     private static long back_pressed;
+    private ActivityMainBinding binding;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+
+    public void init(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
 
     @Override
-    public void onBackPressed() {   //exit from app
+    public void onBackPressed() {   //exit from the app
         if (back_pressed + 2000 > System.currentTimeMillis()) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_HOME);
