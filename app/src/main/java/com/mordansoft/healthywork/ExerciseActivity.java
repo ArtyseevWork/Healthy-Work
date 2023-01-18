95e37933d6bfe5c668dcdbac105120a15cc68e17
ArtyseevWork <ArtyseevWork@gmail.com>
2023-01-18 12:32:19 +0200

    feature exercise add and spinner
    add buttons for change count
    add spinner for choose units

--- app/src/main/java/com/mordansoft/healthywork/ExerciseActivity.java
+++ app/src/main/java/com/mordansoft/healthywork/ExerciseActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+
 import com.mordansoft.healthywork.databinding.ActivityExerciseBinding;
 
 public class ExerciseActivity extends AppCompatActivity {
@@ -33,12 +35,14 @@ public class ExerciseActivity extends AppCompatActivity {
         int exerciseId = arguments.getInt("EXTRA_EXERCISE_ID");
         this.exercise = Exercise.getExerciseById(this, exerciseId);
         binding.btnNewExercise.setOnClickListener(btnSaveListener);
+        binding.btnExerciseMinusCount.setOnClickListener(btnMinusListener);
+        binding.btnExercisePlusCount.setOnClickListener(btnPlusListener);
     }
 
     private void updateUi() {
         try {
             binding.etExerciseName.setText(exercise.getName());
-            binding.etExerciseUnits.setText(exercise.getUnit());
+            binding.etExerciseUnits.setSelection(getIndex(binding.etExerciseUnits, exercise.getUnit()));
             binding.etExerciseCounts.setText(String.valueOf(exercise.getCount()));
             binding.chbExerciseAvailable.setChecked(exercise.isEnable());
             binding.etExerciseDescription.setText(exercise.getDescription());
@@ -47,15 +51,45 @@ public class ExerciseActivity extends AppCompatActivity {
         }
     }
 
+
+    /********** Listeners **********/
     View.OnClickListener btnSaveListener = this::saveState;
+
+    View.OnClickListener btnPlusListener = v -> {
+        changeCount(1);
+    };
+
+    View.OnClickListener btnMinusListener = v -> {
+        changeCount(-1);
+    };
+
+    @Override
+    public void onBackPressed() {
+        goBack();
+    }
+    /* ****** ! Listeners **********/
+
+    private void changeCount(int delta){
+        int count = Integer.parseInt(String.valueOf(binding.etExerciseCounts.getText())) + delta;
+        binding.etExerciseCounts.setText(String.valueOf(count) );
+    }
 
     private void saveState(View v){
         exercise.setName(String.valueOf(binding.etExerciseName.getText()));
-        exercise.setUnit(String.valueOf(binding.etExerciseUnits.getText()));
+        exercise.setUnit(binding.etExerciseUnits.getSelectedItem().toString());
         exercise.setCount(Integer.parseInt(String.valueOf(binding.etExerciseCounts.getText())));
         exercise.setEnable(binding.chbExerciseAvailable.isChecked());
         exercise.setDescription(String.valueOf(binding.etExerciseDescription.getText()));
         exercise.saveExercise(this);
+    }
+
+    private int getIndex(Spinner spinner, String myString){
+        for (int i=0;i<spinner.getCount();i++){
+            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(myString)){
+                return i;
+            }
+        }
+        return 0;
     }
 
     public void goBack() {
@@ -63,10 +97,7 @@ public class ExerciseActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    public void onBackPressed() {
-        goBack();
-    }
+
 
 
 
