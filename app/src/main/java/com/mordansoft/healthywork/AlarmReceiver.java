899a843111eae2a6bbbf462325eeb576c9e83040
ArtyseevWork <ArtyseevWork@gmail.com>
2023-01-22 17:31:28 +0200

    feature add sound and vibration at notification time

--- app/src/main/java/com/mordansoft/healthywork/AlarmReceiver.java
+++ app/src/main/java/com/mordansoft/healthywork/AlarmReceiver.java
@@ -3,9 +3,12 @@ package com.mordansoft.healthywork;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
 
 public class AlarmReceiver extends BroadcastReceiver {
-
     CurrentStatus currentStatus;
 
     @Override
@@ -15,7 +18,17 @@ public class AlarmReceiver extends BroadcastReceiver {
         if (currentStatus.getApplicationStatus() == CurrentStatus.applicationStatusActive) {
             ItsTimeNotification notification = new ItsTimeNotification(context);
             notification.createNotification();
+
+
         }
+
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(1000);
+
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
         MordanSoftLogger.addLog("AlarmReceiver END");
     }
 }
